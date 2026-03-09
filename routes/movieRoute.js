4fd82ca07f6311bc719dcc83a4af2e5d7dea53bd
Yoni190 <yfisuhu@gmail.com>
2026-03-09 21:25:37 +0300

    Map /create to movieController

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -1,4 +1,8 @@
 const { Router } = require('express')
+const movieController = require('../controllers/movieController')
 
 const router = Router()
 
+router.post('/create', movieController.create())
+
+module.exports = router
