a2fecdd78365626d97bf8b067619e3f4e0d46f4e
Yoni190 <yfisuhu@gmail.com>
2026-03-09 21:24:57 +0300

    Map director/create to directorController

--- routes/directorRoute.js
+++ routes/directorRoute.js
@@ -1,4 +1,8 @@
 const { Router } = require('express')
+const directorController = require('../controllers/directorController')
 
 const router = Router()
 
+router.post('/create', directorController.create())
+
+module.exports = router
