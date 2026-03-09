f406d1ff070b38a1492d835cfe65e708f8bbc8fa
Yoni190 <yfisuhu@gmail.com>
2026-03-09 21:03:19 +0300

    Import routes in app file

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const movieRoute = require('./routes/movieRoute')
+const categoryRoute = require('./routes/categoryRoute')
+const directorRoute = require('./routes/directorRoute')
 
 const app = express()
 
+app.use('/', movieRoute)
+app.use('/category', categoryRoute)
+app.use('/director', directorRoute)
 
 
 const PORT = process.env.PORT || 3000
