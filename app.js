95d1147fdca776d3d0cca69e6bb8967c4fb2d751
Yoni190 <yfisuhu@gmail.com>
2026-03-09 22:51:59 +0300

    Enable the server to accept json

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const directorRoute = require('./routes/directorRoute')
 const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
 app.use('/', movieRoute)
 app.use('/categories', categoryRoute)
