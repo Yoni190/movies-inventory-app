2d2a37e2acd5244da9d234ccd1c9c959106b5fff
Yoni190 <yfisuhu@gmail.com>
2026-03-09 21:47:12 +0300

    Create query to get movies

---
+++ db/queries.js
@@ -0,0 +1,13 @@
+const { get } = require('../routes/movieRoute')
+const { pool } = require('./pool')
+
+async function getMovies() {
+    const { rows } = await pool.query('SELECT * FROM movies')
+    return rows
+}
+
+
+
+module.exports = [
+    getMovies
+]
