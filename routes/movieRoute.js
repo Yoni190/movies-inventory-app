c868c6751697c34091eeb0e5b607f2ce6bb6f49c
Yoni190 <yfisuhu@gmail.com>
2026-03-09 22:29:35 +0300

    Create route to get movies form

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -5,5 +5,6 @@ const router = Router()
 
 router.get('/', movieController.getMovies)
 router.post('/create', movieController.create)
+router.get('/create', movieController.getCreateForm)
 
 module.exports = router
