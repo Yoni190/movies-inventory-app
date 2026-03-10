8df6d01865b9726fb628d2ee97237c6c4cc08c3f
Yoni190 <yfisuhu@gmail.com>
2026-03-10 19:40:08 +0300

    Use middleware to utilize styles

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', movieRoute)
 app.use('/categories', categoryRoute)
