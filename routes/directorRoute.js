2aeaff06c741e4ff6462daec57999a29ffad8d82
Yoni190 <yfisuhu@gmail.com>
2026-03-10 12:30:13 +0300

    Create route to edit directors

--- routes/directorRoute.js
+++ routes/directorRoute.js
@@ -6,5 +6,6 @@ const router = Router()
 router.get('/', directorController.getDirectors)
 router.post('/create', directorController.create)
 router.get('/create', directorController.getCreateForm)
+router.get('/:director_id/edit', directorController.getEditForm)
 
 module.exports = router
