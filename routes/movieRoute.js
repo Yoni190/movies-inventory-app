9d67489c2f4ed50acedaa1cd44a129f9ec714991
Yoni190 <yfisuhu@gmail.com>
2026-03-10 12:59:36 +0300

    Create route to get edit form

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -6,5 +6,6 @@ const router = Router()
 router.get('/', movieController.getMovies)
 router.post('/create', movieController.create)
 router.get('/create', movieController.getCreateForm)
+router.get('/:movie_id/edit', movieController.getEditForm)
 
 module.exports = router
