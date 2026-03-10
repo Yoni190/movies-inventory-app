f69ec9fcf6c67ee4413c6a05f99b8feb0d3684e6
Yoni190 <yfisuhu@gmail.com>
2026-03-10 19:12:18 +0300

    Create route to delete categories

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -8,5 +8,6 @@ router.post('/create', categoryController.create)
 router.get('/create', categoryController.getCreateForm)
 router.get('/:category_id/edit', categoryController.getEditForm)
 router.post('/:category_id/edit', categoryController.edit)
+router.post('/:category_id/delete', categoryController.deleteCategory)
 
 module.exports = router
