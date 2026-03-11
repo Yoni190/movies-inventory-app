3f51a865f1684e7b6b1c701c648bba543be4a890
Yoni190 <yfisuhu@gmail.com>
2026-03-11 21:06:24 +0300

    Use connectionString to connect to DB

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,5 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 module.exports = new Pool({
-    host: process.env.DBHOSt,
-    user: process.env.ROLENAME,
-    database: process.env.DBNAME,
-    password: process.env.ROLEPASSWORD,
-    port: process.env.DBPORT
+    connectionString: `postgresql://${process.env.ROLENAME}:${process.env.ROLEPASSWORD}@${process.env.DBHOST}/${process.env.DBNAME}?sslmode=require&channel_binding=require`
 })
